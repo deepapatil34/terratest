63e41c255e81a6d3e9934055da57ec62d6a5f23b
deepapatil34 <81512753+deepapatil34@users.noreply.github.com>
2021-05-07 16:55:17 +0530

    update

---
+++ provider.tf
@@ -0,0 +1,4 @@
+provider "aws" {
+access_key = var.AWS_SECRET_KEY
+aws_region = var.AWS_REGION
+}
---
+++ vars.tf
@@ -0,0 +1,8 @@
+variable "AWS_REGION" {
+    type = string
+	default = "ap-south-1"
+	}
+	
+variable "AWS_SECRET_KEY" {
+	description = "enter value"
+	}
